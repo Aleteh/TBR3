819d6147e6a0b7433c9ff09c35e4fc219b14d8b1
MNoya <martinnoya@gmail.com>
2014-11-05 14:16:24 -0200

    Spartan Warrior mostly finished
    http://giant.gfycat.com/EvilThirstyKoalabear.gif

--- scripts/vscripts/abilities/spartan.lua
+++ scripts/vscripts/abilities/spartan.lua
@@ -82,7 +82,6 @@ function spartan_throw( event ) -- not to be confused with 300, which was tactic
 end
 
 function javelin_particle ( event )
-    print("creating particle effect")
     local javelin_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_chaos_knight/chaos_knight_chaos_bolt_explosion.vpcf", PATTACH_ABSORIGIN_FOLLOW, event.target)
     ParticleManager:SetParticleControl(javelin_particle, 3, event.target:GetAbsOrigin())
     ParticleManager:SetParticleControl(javelin_particle, 1, event.target:GetAbsOrigin())
@@ -96,3 +95,159 @@ function phalanx( event )
         event.ability:ToggleAbility()
     end
 end
+
+function charge( event )
+    local target = event.target
+    local hero = event.caster
+    local single_target_damage = event.ability:GetAbilityDamage() + ( hero:GetStrength() )
+    local spellPower = hero.spellPower
+    local radius = event.ability:GetLevelSpecialValueFor("radius", (event.ability:GetLevel()-1))
+    local aoe_damage = event.ability:GetLevelSpecialValueFor("aoe_damage", (event.ability:GetLevel()-1)) + ( hero:GetStrength() / 2 )
+    local daze_duration = event.ability:GetLevelSpecialValueFor("daze_duration", (event.ability:GetLevel()-1))
+    local taunt_duration = event.ability:GetLevelSpecialValueFor("taunt_duration", (event.ability:GetLevel()-1))
+
+    -- charge physics
+    local unit = event.caster
+    
+    Physics:Unit(unit)
+    
+    if target==nil then
+      return
+    end
+    
+    unit:PreventDI(true)
+    unit:SetAutoUnstuck(false)
+    unit:SetNavCollisionType(PHYSICS_NAV_NOTHING)
+    unit:FollowNavMesh(false)
+    
+    local distance = target:GetAbsOrigin() - unit:GetAbsOrigin()
+    local direction = target:GetAbsOrigin() - unit:GetAbsOrigin()
+    direction = direction:Normalized()
+    unit:SetPhysicsFriction(0)
+    
+    local gravity = 1
+    local velocity = 1000
+    local timetotarget = distance:Length() / velocity
+    local jump = gravity * (timetotarget) * -1
+    --jump = jump/2
+    
+    unit:SetPhysicsVelocity(direction * velocity)
+    
+    unit:AddPhysicsAcceleration(Vector(0,0,gravity))
+    unit:AddPhysicsVelocity(Vector(0,0,jump))
+    
+    Timers:CreateTimer(math.min(timetotarget, .5),
+    function()
+      local groundpos = GetGroundPosition(unit:GetAbsOrigin(), unit)
+      if unit:GetAbsOrigin().z - groundpos.z <= 20 then
+          unit:SetPhysicsAcceleration(Vector(0,0,0))
+          unit:SetPhysicsVelocity(Vector(0,0,0))
+          unit:OnPhysicsFrame(nil)
+          unit:PreventDI(false)
+          unit:SetNavCollisionType(PHYSICS_NAV_SLIDE)
+          unit:SetAutoUnstuck(true)
+          unit:FollowNavMesh(true)
+          unit:SetPhysicsFriction(.05)
+          FindClearSpaceForUnit(unit, target:GetAbsOrigin(), true)
+
+          -- Do the Damage and effects after the movement is completed
+           -- The charged unit will suffer xxxx damage plus your Strength
+            hero:MoveToTargetToAttack(target)
+            ApplyDamage({
+                victim = target,
+                attacker = hero,
+                damage = single_target_damage + spellPower,
+                damage_type = DAMAGE_TYPE_MAGICAL
+            })
+
+            -- All nearby units will suffer xxxx damage plus half your Strength and will be dazed for up to 5 seconds. 
+            -- Find enemies
+            enemies = FindUnitsInRadius(event.caster:GetTeamNumber(),
+                                  target:GetAbsOrigin(),
+                                  nil,
+                                  radius,
+                                  DOTA_UNIT_TARGET_TEAM_ENEMY,
+                                  DOTA_UNIT_TARGET_ALL,
+                                  DOTA_UNIT_TARGET_FLAG_NONE,
+                                  FIND_ANY_ORDER,
+                                  false)
+
+            -- Do damage in AoE
+            for _,enemy in pairs(enemies) do
+                if enemy ~= target then -- exclude the original target
+                    ApplyDamage({
+                        victim = enemy,
+                        attacker = event.caster,
+                        damage =  aoe_damage + spellPower,
+                        damage_type = DAMAGE_TYPE_MAGICAL
+                        })
+                end
+                -- While dazed, the affected units will have their movement and attack speed reduced by 50%.
+                event.ability:ApplyDataDrivenModifier(hero, enemy, "modifier_charge_daze", {duration = daze_duration})
+            end
+
+            -- If used against a unit in melee range, the target will be forced to attack you for 4 seconds. 
+            -- I'll ignore the unit in melee range stuff for now, feels like a pointless limitation.
+            event.ability:ApplyDataDrivenModifier(hero, target, "modifier_spartan_taunt", {duration = taunt_duration})
+          return nil
+      end
+      return 0.01
+    end)
+   
+end
+
+function taunt( event )
+    local taunt_duration = event.ability:GetLevelSpecialValueFor("taunt_duration", (event.ability:GetLevel()-1))
+
+    event.target:SetForceAttackTarget(event.caster)
+   
+    local particle = ParticleManager:CreateParticle("particles/econ/items/dragon_knight/dk_immortal_dragon/dragon_knight_dragon_tail_dragonform_iron_dragon.vpcf", PATTACH_OVERHEAD_FOLLOW, event.target)
+    ParticleManager:SetParticleControl(particle, 4, event.target:GetAbsOrigin())
+    ParticleManager:SetParticleControl(particle, 2, event.caster:GetAbsOrigin())
+    ParticleManager:SetParticleControl(particle, 1, event.caster:GetAbsOrigin())
+
+    Timers:CreateTimer({
+    endTime = 1,
+    callback = function()
+        local particle = ParticleManager:CreateParticle("particles/econ/items/dragon_knight/dk_immortal_dragon/dragon_knight_dragon_tail_dragonform_iron_dragon.vpcf", PATTACH_OVERHEAD_FOLLOW, event.target)
+        ParticleManager:SetParticleControl(particle, 4, event.target:GetAbsOrigin())
+    end
+    })
+
+    Timers:CreateTimer({
+    endTime = 2,
+    callback = function()
+        local particle = ParticleManager:CreateParticle("particles/econ/items/dragon_knight/dk_immortal_dragon/dragon_knight_dragon_tail_dragonform_iron_dragon.vpcf", PATTACH_OVERHEAD_FOLLOW, event.target)
+        ParticleManager:SetParticleControl(particle, 4, event.target:GetAbsOrigin())
+    end
+    })
+
+   Timers:CreateTimer({
+    endTime = 3,
+    callback = function()
+        local particle = ParticleManager:CreateParticle("particles/econ/items/dragon_knight/dk_immortal_dragon/dragon_knight_dragon_tail_dragonform_iron_dragon.vpcf", PATTACH_OVERHEAD_FOLLOW, event.target)
+        ParticleManager:SetParticleControl(particle, 4, event.target:GetAbsOrigin())
+    end
+    })
+
+    Timers:CreateTimer({
+    endTime = taunt_duration,
+    callback = function()
+      event.target:SetForceAttackTarget(nil)
+    end
+    })
+end
+
+
+
+
+
+-- I'll just keep here this for later
+function SpinHero(keys)
+    local caster = keys.caster
+    local total_degrees = keys.Angle
+    print("Spinning ", total_degrees, "degrees about center")
+    caster:SetForwardVector(RotatePosition(Vector(0,0,0), QAngle(0,total_degrees,0), caster:GetForwardVector()))
+end
+-- Taken from https://github.com/Dli42/Dota_Hero_Script_Testing/blob/f9db344555adff7cf4d78f67e576be98a517cc3a/scripts/vscripts/custom_scripted_abilities.lua
+-- many good stuff to learn from there
