1ee84545951fd79613fe4d04c6b3d38bf9192a59
rockonrob <williamk03hler@gmail.com>
2014-11-10 18:58:11 -0600

    Commiting before things go wrong

--- scripts/vscripts/abilities/cleric.lua
+++ scripts/vscripts/abilities/cleric.lua
@@ -1,11 +1,13 @@
 function holy_light(event)
 	event.target:EmitSound("Hero_Omniknight.Purification")
 	local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_omniknight/omniknight_purification.vpcf", PATTACH_ABSORIGIN_FOLLOW, event.target)
-	local heal_amount = event.ability:GetLevelSepcialValueFor("heal_amount", (event.ability:GetLevel()-1)) + event.caster.spellPower
+	local heal_amount = event.ability:GetLevelSpecialValueFor("heal_amount", (event.ability:GetLevel()-1)) + event.caster.spellPower
 	event.target:Heal( heal_amount, event.caster)
-	PopupHealing(even.target, heal_amount)
+	PopupHealing(event.target, heal_amount)
 end
 
 function regen(event)
-	local heal_amount = 1
+	local heal_amount = (event.caster.healingPower + event.ability:GetLevelSpecialValueFor("heal_amount", (event.ability:GetLevel1()-1))) / event.ability:GetSpecialValueFor("duration")
+	event.target:Heal(heal_amount, event.caster)
+	Popuphealing(event.target, heal_amount)
 end
