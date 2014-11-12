475b41418a8316ce8d156cc7d5fa99f9d56b28fe
MNoya <martinnoya@gmail.com>
2014-11-12 17:02:39 -0200

    Ranger Particles almost done
    Missing a pair of sounds

--- scripts/vscripts/abilities/ranger.lua
+++ scripts/vscripts/abilities/ranger.lua
@@ -16,6 +16,8 @@ function black_arrow( event )
 			ParticleManager:SetParticleControl(particle, 2, Vector(radius,radius,1)) --radius
 			ParticleManager:SetParticleControl(particle, 3, Vector(radius,radius,1)) --other radius
 
+			local particle = ParticleManager:CreateParticle("particles/econ/items/vengeful/vengeful_weapon_talon/vengeful_wave_of_terror_glow_c_talon.vpcf", PATTACH_ABSORIGIN_FOLLOW, dummy)
+
 			local group = FindUnitsInRadius( event.caster:GetTeamNumber(), event.target:GetAbsOrigin(), nil, event.ability:GetSpecialValueFor("explosion_radius"), DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
 			for key, unit in pairs(group) do
 				ApplyDamage({ victim = unit, attacker = event.caster, damage = event.ability:GetAbilityDamage(), damage_type = event.ability:GetAbilityDamageType(), ability = event.ability	})
@@ -58,14 +60,38 @@ function puncture_bleed( event )
 end
 
 function star_dust( event )
-	event.ability:ApplyDataDrivenModifier( event.caster, event.target, "ranger_star_dust_modifier", nil)
+	local target = event.target
+	local hero = event.caster
+	local radius = event.ability:GetSpecialValueFor("heal_radius")
+	local damage = event.ability:GetAbilityDamage() + hero.spellPower
+
+	--Deal damage and apply star dust on nearby friendly units to 
+	ApplyDamage({ victim = target, attacker = hero, damage = damage, damage_type = DAMAGE_TYPE_MAGICAL, ability = event.ability	})
+
+	-- TODO: Check if this damage should be AoE. Sounds like it should.
+
+	local group = FindUnitsInRadius( hero:GetTeamNumber(), target:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, 
+										DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+	
+	-- Apply the healing modifier on every ally around
+	for _,unit in pairs(group) do
+		event.ability:ApplyDataDrivenModifier( hero, unit, "ranger_star_dust_modifier", nil)
+	end		
 end
 
 function star_dust_heal( event )
-	local group = FindUnitsInRadius( event.caster:GetTeamNumber(), event.target:GetAbsOrigin(), nil, event.ability:GetSpecialValueFor("heal_radius"), DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
-	for key, unit in pairs(group) do
-		unit:Heal((event.ability:GetAbilityDamage() / 3), event.caster)
-	end
+	-- Heal them for that much health over 3 seconds for xxxx mana.
+	-- TODO: Check if this should use healing Power, I'm assuming it does.
+	Timers:CreateTimer({
+	    endTime = 0.5,
+	    callback = function()
+	    	local healingPower = event.caster.healingPower
+			local healing_amount = (event.ability:GetAbilityDamage() + healingPower) / 3 
+			event.target:Heal(healing_amount, event.caster)
+			PopupHealing(event.target, healing_amount)
+	    end
+	})
+
 end
 
 function soul_piercing_shot( event )
