f8ab48de1ac0595900aaead1aad209d783406bfd
MNoya <martinnoya@gmail.com>
2014-11-14 00:22:27 -0200

    Made proper Drain Particles
    Thanks BMD-God

--- scripts/vscripts/abilities/warlock.lua
+++ scripts/vscripts/abilities/warlock.lua
@@ -21,15 +21,47 @@ function drain_fx(event)
 	local hero = event.caster
 	local target = event.target
 
-	target.drain_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_pugna/pugna_life_drain.vpcf", PATTACH_OVERHEAD_FOLLOW, target)
-    ParticleManager:SetParticleControl(target.drain_particle, 0, target:GetAbsOrigin()) --coordinates of the sucking
-    ParticleManager:SetParticleControl(target.drain_particle, 1, Vector(hero:GetAbsOrigin().x,hero:GetAbsOrigin().y,hero:GetAbsOrigin().z+100)) --where to suck to
-    --ParticleManager:SetParticleControl(particle, 10, Vector(0,0,0))
-    --ParticleManager:SetParticleControl(particle, 11, Vector(0,0,0))
+	--hero.drain_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_pugna/pugna_life_drain.vpcf", PATTACH_CUSTOMORIGIN, hero)
+	--<BMD> the gist is it's (particleID, CP, entity, dont_remember, attach_type, initial point, dont_remember)
+	--ParticleManager:SetParticleControlEnt(hero.drain_particle, 0, hero, 1, "attach_hitloc", hero:GetAbsOrigin(), true)
+  	--ParticleManager:SetParticleControlEnt(hero.drain_particle, 1, target, 1, "attach_mouth", target:GetAbsOrigin(), true)
+    --ParticleManager:SetParticleControl(hero.drain_particle, 10, Vector(1,0,0)) --use green
+    --ParticleManager:SetParticleControl(hero.drain_particle, 11, Vector(0,0,0)) --use blue, false
 
 end
 
 function destroy_drain_fx(event)
+	local hero = event.caster
+	ParticleManager:DestroyParticle(hero.drain_particle,false)
+end
+
+function soul_drain(event)
+	local hero = event.caster
 	local target = event.target
-	ParticleManager:DestroyParticle(target.drain_particle,false)
+	local spellPower = hero.spellPower
+	local drain = event.ability:GetAbilityDamage() + spellPower
+
+	ApplyDamage({ victim = target, attacker = hero, damage = drain, damage_type = event.ability:GetAbilityDamageType(), ability = event.ability })
+	print("drained ",drain," from ",target:GetUnitName())
+	hero:Heal( drain, hero )
+	PopupHealing( hero, drain )
+
+end
+
+function soul_drain_aoe( event )
+	local target = event.target
+	local hero = event.caster
+	local spellPower = hero.spellPower
+	local spell_multiplier = event.ability:GetSpecialValueFor("bonus_spell_dmg")
+	local dmg = spellPower * 0.01 * spell_multiplier
+	local aoe = event.ability:GetSpecialValueFor("aoe")
+
+	-- find enemies nearby
+	enemies = FindUnitsInRadius(hero:GetTeamNumber(), target:GetAbsOrigin(),  nil, aoe, DOTA_UNIT_TARGET_TEAM_ENEMY,
+        DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+
+	-- do damage
+	for _,enemy in pairs(enemies) do
+		ApplyDamage({ victim = enemy, attacker = hero, damage = dmg, damage_type = DAMAGE_TYPE_MAGICAL })
+    end
 end
