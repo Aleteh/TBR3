fb9afa7951aa81e302c3c3a681caeb2cf2001124
MNoya <martinnoya@gmail.com>
2014-11-17 15:24:08 -0200

    Continued Magician

--- scripts/vscripts/abilities/magician.lua
+++ scripts/vscripts/abilities/magician.lua
@@ -1,9 +1,9 @@
 function flame_spire(event)
-	local hero = target.CASTER
-	local spellpower = hero:spellPower
+	local hero = event.caster
+	local spellpower = hero.spellPower
 
 	local aoe = event.ability:GetSpecialValueFor("radius")
-	local duration = event.abilit:GetSpecialValueFor("duration")
+	local duration = event.ability:GetSpecialValueFor("duration")
 	local tick_rate = event.ability:GetSpecialValueFor("tick_interval")
 	local damage_per_tick = 1 / (duration / tick_rate)
 	local aoe_damage =  (event.ability:GetLevelSpecialValueFor("aoe_damage", event.ability:GetLevel()-1) + spellpower) * damage_per_tick
@@ -13,4 +13,22 @@ function flame_spire(event)
 	for _,enemy in pairs(enemies) do
 		ApplyDamage({ victim = enemy, attacker = hero, damage = aoe_damage, damage_type = DAMAGE_TYPE_MAGICAL })
     end
+end
+
+function refresh_cooldowns( event )
+	print("refreshing cooldowns")
+
+  	Timers:CreateTimer({
+    	endTime = 0.1,
+    	callback = function()
+      		local hero = event.caster
+			for i=0,4 do
+				local ability = hero:GetAbilityByIndex(i)
+				--if not ability:IsCooldownReady() then
+				ability:EndCooldown()
+				--end
+			end
+    	end
+  	})
+	
 end
