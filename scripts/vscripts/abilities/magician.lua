01533afe1f9beb343b26f4f4931089ed0b8d917f
MNoya <martinnoya@gmail.com>
2014-11-18 00:16:13 -0200

    Magician Particles work

--- scripts/vscripts/abilities/magician.lua
+++ scripts/vscripts/abilities/magician.lua
@@ -1,23 +1,53 @@
 function frost_bolt( event )
-	local target = event.target_points[1]
+	local caster = event.caster
+
+	local dir = (event.target_points[1] - caster:GetAbsOrigin()):Normalized()
+	local dist = dir * 700
+
+	local position = caster:GetAbsOrigin() + dist
 	
 	-- icewall starting from the caster up to the target point (or use a fixed distance)
-end
+	local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_jakiro/jakiro_ice_path_b.vpcf", PATTACH_ABSORIGIN_FOLLOW, event.caster)
+	ParticleManager:SetParticleControl(particle, 0, caster:GetAbsOrigin()) -- origin
+    ParticleManager:SetParticleControl(particle, 1, position) -- destination
+    ParticleManager:SetParticleControl(particle, 2, Vector(2,0,0)) -- duration
+    ParticleManager:SetParticleControl(particle, 9, caster:GetAbsOrigin()) -- blast origin
+
+    local particle2 = ParticleManager:CreateParticle("particles/units/heroes/hero_invoker/invoker_ice_wall_b.vpcf", PATTACH_ABSORIGIN_FOLLOW, event.caster)
+	ParticleManager:SetParticleControl(particle2, 0, caster:GetAbsOrigin()) -- origin
+    ParticleManager:SetParticleControl(particle2, 1, position) -- destination
+    ParticleManager:SetParticleControl(particle2, 2, Vector(0,0,1)) -- normal
+
+  	Timers:CreateTimer({
+    	endTime = 2,
+    	callback = function()
+      		ParticleManager:DestroyParticle(particle, false)
+      		ParticleManager:DestroyParticle(particle2, false)
+    	end
+  	})
+	end
 
 function flame_spire(event)
 
-	--macropyre
+	local target = event.target:GetAbsOrigin()
 
+	--macropyre
 	local hero = event.caster
-	local spellpower = hero.spellPower
+	local spellPower = hero.spellPower
+
+	local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_jakiro/jakiro_macropyre.vpcf", PATTACH_ABSORIGIN_FOLLOW, event.target)
+	ParticleManager:SetParticleControl(particle, 0, target) -- origin
+	ParticleManager:SetParticleControl(particle, 1, target) -- origin
+    ParticleManager:SetParticleControl(particle, 2, Vector(1,0,0)) -- duration
 
 	local aoe = event.ability:GetSpecialValueFor("radius")
 	local duration = event.ability:GetSpecialValueFor("duration")
 	local tick_rate = event.ability:GetSpecialValueFor("tick_interval")
 	local damage_per_tick = 1 / (duration / tick_rate)
-	local aoe_damage =  (event.ability:GetLevelSpecialValueFor("aoe_damage", event.ability:GetLevel()-1) + spellpower) * damage_per_tick
+	local damage = event.ability:GetLevelSpecialValueFor("damage", event.ability:GetLevel()-1) + spellPower
+	local aoe_damage = damage * damage_per_tick
 
-	enemies = FindUnitsInRadius(event.caster:GetTeamNumber(), target:GetAbsOrigin(),  nil, aoe, DOTA_UNIT_TARGET_TEAM_ENEMY,
+	enemies = FindUnitsInRadius(event.caster:GetTeamNumber(), event.target:GetAbsOrigin(),  nil, aoe, DOTA_UNIT_TARGET_TEAM_ENEMY,
         DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
 	for _,enemy in pairs(enemies) do
 		ApplyDamage({ victim = enemy, attacker = hero, damage = aoe_damage, damage_type = DAMAGE_TYPE_MAGICAL })
