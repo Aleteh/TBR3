1d61926056054cf7d69363bee0cccb22a8436385
Aleteh <dreasf@gmail.com>
2017-01-19 16:57:05 +0100

    Done

--- game/dota_addons/theblackroad3/scripts/vscripts/quest/questTriggerListener.lua
+++
@@ -1,8 +0,0 @@
-
-OnStartTouch = function(keys)
-	Quest.OnTrigger(keys.activator, keys.caller:GetName(), true)
-end
-
-OnEndTouch = function(keys)
-	Quest.OnTrigger(keys.activator, keys.caller:GetName())
-end
